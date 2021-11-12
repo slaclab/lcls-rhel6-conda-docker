c39e479caa1cfb98f5e9ebb2e86a615f7d8680cf
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2021-11-12 09:18:55 -0800

    Fix unpack

--- run-pack-and-test.sh
+++ run-pack-and-test.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 ENVIRONMENT_FILE=$1
 
+# exit when any command fails
+set -e
+
 source /root/.bashrc 
 
+# create an dpack environment
 conda env create -f $ENVIRONMENT_FILE
-conda pack -n $ENVIRONMENT_NAME --ignore-missing-files --output $HOME/$ENVIRONMENT_NAME.tar.gz
+conda pack -n $ENVIRONMENT_NAME --output $HOME/$ENVIRONMENT_NAME.tar.gz
 
+# unpack 
 cp $HOME/$ENVIRONMENT_NAME.tar.gz /tmp/$ENVIRONMENT_NAME.tar.gz
 mkdir /tmp/$ENVIRONMENT_NAME
-tar -xvf /tmp/$ENVIRONMENT_NAME.tar.gz -C /tmp/$ENVIRONMENT_NAME
+tar -xf /tmp/$ENVIRONMENT_NAME.tar.gz -C /tmp/$ENVIRONMENT_NAME
 source /tmp/$ENVIRONMENT_NAME/bin/activate
-bash /tmp/$ENVIRONMENT_NAME/bin/conda-unpack
-
+/tmp/$ENVIRONMENT_NAME/bin/conda-unpack
 
 # run tests
 bash /tmp/run-test.sh
